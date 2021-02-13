2ba2cd05175352e3623c52ddd0486c843a797682
Pedro Zenha <pedrozenha12@gmail.com>
2021-02-13 01:29:21 +0000

    add !elo command

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"regexp"
+	"strconv"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
-var ( Token string )
+var ( 
+	Token string 
+	FaceitAPIKey string
+)
+
+type FaceitResponse struct {
+	Nickname string `json:"nickname,omitempty"`
+	Games Games `json:"games,omitempty"`
+}
+
+type Games struct{
+	Csgo Csgo `json:csgo,omitempty`
+}
+
+type Csgo struct{
+	Skill_level int `json:skill_level,omitempty`
+	Faceit_elo int `json:faceit_elo,omitempty`
+}
 
 func init(){
 
@@ -18,9 +42,14 @@ func init(){
         fmt.Println("No .env file found")
     }
 
-	value, exists := os.LookupEnv("DISCORD_BOT_TOKEN")
+	dbt, exists := os.LookupEnv("DISCORD_BOT_TOKEN")
 	if exists {
-		Token = value
+		Token = dbt
+	}
+
+	fak, exists := os.LookupEnv("FACEIT_API_KEY")
+	if exists {
+		FaceitAPIKey = fak
 	}
 }
 
@@ -54,7 +83,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-
+fmt.Println(m.Content)
 	if m.Content == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
@@ -62,4 +91,47 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
+
+	match, _ := regexp.MatchString("(!elo )[^ ]*", m.Content)
+	fmt.Println(match)
+	if match {
+		nickname := m.Content[5:len(m.Content)]
+		fmt.Println(nickname)
+		elo := faceitElo(nickname)
+		s.ChannelMessageSend(m.ChannelID, strconv.Itoa(elo))
+	}
+}
+
+func faceitElo(nickname string) int{
+	url := "https://open.faceit.com/data/v4/players?nickname="+nickname+"&game=csgo"
+	bearer := "Bearer " + FaceitAPIKey
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", bearer)
+
+	client := &http.Client{}
+	
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error", err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	
+
+	r := bytes.NewReader([]byte(body))
+	decoder := json.NewDecoder(r)
+
+	val := &FaceitResponse{}
+	err = decoder.Decode(val)
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	return val.Games.Csgo.Faceit_elo
 }
